1309f9ca306c1ac4409dc1a2dfc49edc058cff59
Shivani BM <158183372+Shivani-901@users.noreply.github.com>
2024-02-11 19:41:30 +0530

    Add files via upload

---
+++ csv file read.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv("https://raw.githubusercontent.com/ShapeLab/ZooidsCompositePhysicalizations/master/Zooid_Vis/bin/data/student-dataset.csv")
+print(df.head())
+                 
